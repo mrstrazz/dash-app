4180901628e2ed2a2609edc667595034ea78a313
mrstrazz <philstraz@gmail.com>
2025-07-16 10:41:20 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import dash
+from dash import html, dcc
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+# Sample data
+df = px.data.gapminder().query("year == 2007")
+
+fig = px.scatter(df, x="gdpPercap", y="lifeExp", size="pop",
+                 color="continent", hover_name="country", log_x=True)
+
+app.layout = html.Div([
+    html.H1("Gapminder 2007"),
+    dcc.Graph(figure=fig)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
